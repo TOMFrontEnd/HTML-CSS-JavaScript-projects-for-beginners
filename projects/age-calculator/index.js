817bf4532a8086d094b00e811ae195e0e961936c
tom gu <tomgu7035@gmail.com>
2023-04-30 16:52:34 +1000

    add months and days

--- projects/age-calculator/index.js
+++ projects/age-calculator/index.js
@@ -4,28 +4,43 @@ const resultEl = document.getElementById("result");
 
 function calculateAge() {
   const birthdayValue = birthdayEl.value;
+  // console.log(birthdayValue)
   if (birthdayValue === "") {
     alert("Please enter your birthday");
   } else {
     const age = getAge(birthdayValue);
-    resultEl.innerText = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+    resultEl.innerText = `Your age is ${age.years=0?'':age.years} ${"years"} ${age.months=0?'':age.months} ${"months"}    ${age.days=0?"":age.days} ${"days"} old`;
   }
 }
+// here cancel 0 display doesn'gitt work
 
 function getAge(birthdayValue) {
-  const currentDate = new Date();
-  const birthdayDate = new Date(birthdayValue);
-  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
-  const month = currentDate.getMonth() - birthdayDate.getMonth();
-
-  if (
-    month < 0 ||
-    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
-  ) {
-    age--;
+  
+    const today = new Date();
+    const birth = new Date(birthdayValue);
+    let age = {};
+  
+    let yearDiff = today.getFullYear() - birth.getFullYear();
+    let monthDiff = today.getMonth() - birth.getMonth();
+    let dayDiff = today.getDate() - birth.getDate();
+  
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+      yearDiff--;
+      if (monthDiff < 0) {
+        monthDiff += 12;
+      }
+      if (dayDiff<0) {
+      const lastMonth = new Date(today.getFullYear(), today.getMonth() - 1, 0);
+    dayDiff += lastMonth.getDate();}
+      // dayDiff = today.getDate() + (new Date(today.getFullYear(), today.getMonth(), 0)).getDate() - birth.getDate();
+    }
+  
+    age.years = yearDiff;
+    age.months = monthDiff;
+    age.days = dayDiff;
+  
+    return age;
   }
 
-  return age;
-}
 
 btnEl.addEventListener("click", calculateAge);
